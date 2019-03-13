d3ec7f85614cc19adb487fc88f892f789c1b8a5e
Andriy Zhyzhenko <A.Zhyzhenko@favbet.com>
2019-03-13 18:54:14 +0200

    ts filter aside

--- src/app/aside-filter/aside-filter.component.ts
+++ src/app/aside-filter/aside-filter.component.ts
@@ -7,19 +7,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AsideFilterComponent implements OnInit {
 
+  public rangeValue: number;
   constructor() { }
 
   ngOnInit() {
   }
 
   public openFilter(event: MouseEvent | any): void {
-    const parentFilterBlock: HTMLDivElement = event.target.parentNode;
-    const filterButtonHeight: number = event.target.getBoundingClientRect().height;
-    const filterUlHeight: number =  event.target.nextElementSibling.getBoundingClientRect().height;
-    parentFilterBlock.style.height = (filterButtonHeight + filterUlHeight) + 30 + "px";
-    console.dir(event.target);
-    console.dir(event.target.nextElementSibling);
-    console.dir(parentFilterBlock);
+    if(event.target.nodeName === "BUTTON"){
+      
+      const parentFilterBlock: HTMLDivElement = event.target.parentNode;
+      const filterButtonHeight: number = event.target.getBoundingClientRect().height;
+      const filterUlHeight: number =  event.target.nextElementSibling.getBoundingClientRect().height;
+      const arrow: HTMLSpanElement = event.target.childNodes[1];
 
+      arrow.classList.toggle("up-arrow");
+
+
+      if(parentFilterBlock.getBoundingClientRect().height > filterButtonHeight){
+        parentFilterBlock.style.height = filterButtonHeight + "px";
+      }else{
+        parentFilterBlock.style.height = (filterButtonHeight + filterUlHeight) + "px";
+      }
+      
+      // console.dir(event.target);
+      // console.dir(arrow);
+    }
+
+    console.dir(event.target);
   }
+
+  public changeFilterRange(rangeValue: number, event): void{
+    this.rangeValue = rangeValue;
+    const maxMoney: HTMLSpanElement = event.target.previousSibling;
+    const maxMoneyPosition: number = rangeValue / (event.target.max / 100);
+    // console.log(this.rangeValue);
+    // console.dir(event.target.getBoundingClientRect().width);
+    // console.dir(event.target.max);
+    // console.log(rangeValue);
+    // console.dir(event.target.max / 100);
+    maxMoney.style.left = "calc(" + maxMoneyPosition + "% - 0px)";
+    // console.dir(rangeValue / (event.target.max / 100));
+    // console.dir(event.target);
+  }
+
 }
